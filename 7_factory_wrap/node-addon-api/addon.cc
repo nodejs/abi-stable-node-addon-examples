29bd7247dd47499a39f29f33f72a9be7ea1da2f1
Anisha Rohra <Anisha.Rohra@ibm.com>
2017-09-25 12:02:11 -0400

    Added working example 7

---
+++ 7_factory_wrap/node-addon-api/addon.cc
@@ -0,0 +1,14 @@
+#include <napi.h>
+#include <uv.h>
+#include "myobject.h"
+
+Napi::Object CreateObject(const Napi::CallbackInfo& info) {
+  return MyObject::NewInstance(info.Env(), info[0]);
+}
+
+Napi::Object InitAll(Napi::Env env, Napi::Object exports) {
+  MyObject::Init(env, exports);
+  return Napi::Function::New(env, CreateObject, "CreateObject");
+}
+
+NODE_API_MODULE(addon, InitAll)
